584794f403743ffdced771729fad63fb15cbb171
LasBoi <Azlj1689gk>
2019-09-27 18:35:28 +0200

    Lavet andet test case til programmet

--- src/Generator.java
+++ src/Generator.java
@@ -1,8 +1,18 @@
 public class Generator {
+    static int number = 2;
+    static String symbol;
 
-    public static int stenSaksPapir() {
-        int number = 2;
+    public static int stenSaksPapirNumber() {
         return number;
     }
 
+    public static String stenSaksPapirString() {
+        if (number == 1)
+            symbol = "Sten";
+        else if (number == 2)
+            symbol = "Saks";
+        else
+            symbol = "Papir";
+        return symbol;
+    }
 }
--- test/GeneratorTest.java
+++ test/GeneratorTest.java
@@ -6,10 +6,19 @@ public class GeneratorTest extends TestCase {
     //Input x = Generator.stenSaksPapir()
     //Forventet output: x = 2
     public void testStenSaksPapir() {
-        int x = Generator.stenSaksPapir();
+        int x = Generator.stenSaksPapirNumber();
 
         assertEquals(2, x);
 
+    }
+
+    //Test Case - Får vi den rigtige betegnelse
+    //Input: output = Generator.stenSaksPapirString()
+    //Forventet output: output = "Saks"
+    public void testString() {
+        String output = Generator.stenSaksPapirString();
+
+        assertEquals("Saks", output);
     }
 
 }
