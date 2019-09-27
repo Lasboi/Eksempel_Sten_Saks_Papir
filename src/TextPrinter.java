e4f445fc93ff5b2ad4d27509e771faccaaf7f002
LasBoi <Azlj1689gk>
2019-09-27 18:44:28 +0200

    Oprettet TextPrinter klassen for at dele koden bedre op

---
+++ src/TextPrinter.java
@@ -0,0 +1,14 @@
+public class TextPrinter {
+
+    public static void printLines() {
+        System.out.println("******************************");
+    }
+
+    public static void setupProgram() {
+        System.out.println("Velkommen til Sten, Saks Papir!");
+        System.out.println("I dette lille program skal du spille mod Computeren!");
+        System.out.println("Start ud med at vælge Sten, Saks eller Papir og håb på det bedste!");
+        System.out.println("1 = Sten, 2 = Saks, 3 = Papir. Hvad vælger du?: ");
+    }
+
+}
